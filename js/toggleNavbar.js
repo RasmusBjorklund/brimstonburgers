3e1b12d85060490569ff4c13609d293331dcfa50
RasmusBjorklund <45294367+RasmusBjorklund@users.noreply.github.com>
2019-03-04 22:25:09 +0100

    Mobile frendly fixes

--- js/toggleNavbar.js
+++ js/toggleNavbar.js
@@ -10,3 +10,21 @@ $(window).scroll(function () {
   // $('.hero-img').toggleClass('invert', $(this).scrollTop() > 600 );
 
 });
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 190 || document.documentElement.scrollTop > 190) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
+  }
+}
+
+// När man klickar på knappen så tas man till toppen utav hemsidan
+$(function(){
+  $("#backToTop").click(function(e){
+    e.preventDefault();
+    $("html, body").animate({"scrollTop": "0px"});
+  })
+});
